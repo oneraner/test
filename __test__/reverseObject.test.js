e46d4171c830d0ffa9f43cf96c9c152b34695de2
oneraner <oneraner@gmail.com>
2021-07-14 14:00:30 +0800

    增加範例test

--- __test__/reverseObject.test.js
+++ __test__/reverseObject.test.js
@@ -4,4 +4,23 @@ test("have reverse object", () => {
   expect(reverseObject({})).toBe("error");
   expect(reverseObject("ccc")).toBe("error");
   expect(reverseObject({ a: "b" })).toMatchObject({ b: "a" });
+  expect(
+    reverseObject({
+      hired: {
+        be: {
+          to: {
+            deserve: "I",
+          },
+        },
+      },
+    })
+  ).toMatchObject({
+    I: {
+      deserve: {
+        to: {
+          be: "hired",
+        },
+      },
+    },
+  });
 });
